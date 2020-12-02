db37c41549ca5a4717594a3e2319950bcc1fbc39
Cassio Alves <cassio01>
2020-12-02 10:38:15 -0300

    teste

--- src/br/com/kdev/gerenciador/servlet/AlteraEmpresaServlet.java
+++ src/br/com/kdev/gerenciador/servlet/AlteraEmpresaServlet.java
@@ -25,7 +25,7 @@ public class AlteraEmpresaServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		System.out.println("AlteraEmpresaServlet");
 		String id = request.getParameter("id");
 		String nome = request.getParameter("nome");
 		String dataAberturaEmpresa = request.getParameter("data");
